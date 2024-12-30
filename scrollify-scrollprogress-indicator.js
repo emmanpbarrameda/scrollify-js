7938a41b34a5b1f04bf24cbd7cfe389a63445d83
EMMAN <emmanuelbarrameda1@gmail.com>
2024-12-30 19:06:56 +0800

    123024 706PM

--- scrollify-scrollprogress-indicator.js
+++ scrollify-scrollprogress-indicator.js
@@ -28,3 +28,24 @@ window.addEventListener('scroll', function () {
     const scrolled = (scrollTop / scrollHeight) * 100;
     progressBar.style.width = `${scrolled}%`;
 });
+
+
+/* =================== LOAD EVENT =================== */
+window.addEventListener('load', function () {
+    const progressBar = document.querySelector('.scrollify_scroll_progress');
+
+    // custom data attributes
+    const height = progressBar.dataset.height || '4px';
+    const background = progressBar.dataset.background || 'linear-gradient(to left, #4E73DF, #4da3ff)';
+    const zIndex = progressBar.dataset.zIndex || '999';
+    const top = progressBar.dataset.top || '89px';
+
+    // attributes to the progress bar
+    progressBar.style.height = height;
+    progressBar.style.background = background;
+    progressBar.style.zIndex = zIndex;
+    progressBar.style.top = top;
+
+    // Reset the width on page load
+    progressBar.style.width = '0%';
+});
